3ee38ce4194eedaba267092efb4bfdf220166795
Steven Mulvey <33791425+smulvey2@users.noreply.github.com>
2020-04-04 03:44:48 -0500

    Tutorial Level Textboxes

--- PhaserGame/Assets/Scripts/TextTrigger.cs
+++ PhaserGame/Assets/Scripts/TextTrigger.cs
@@ -6,10 +6,27 @@ using UnityEngine;
 public class TextTrigger : MonoBehaviour
 {
     public TextMeshProUGUI colors;
+    public TextMeshProUGUI welcome;
+    public TextMeshProUGUI jump;
+    public TextMeshProUGUI spike;
+    public TextMeshProUGUI spikeBack;
+    public TextMeshProUGUI checkpoint;
+    public TextMeshProUGUI platforms;
+    public TextMeshProUGUI spikeShooters;
+    public TextMeshProUGUI end;
+    public TextMeshProUGUI colorful;
+    private bool checkp = false;
     // Start is called before the first frame update
     void Start()
     {
-        colors.enabled = false;
+        jump.enabled = false;
+        spike.enabled = false;
+        spikeBack.enabled = false;
+        checkpoint.enabled = false;
+        platforms.enabled = false;
+        spikeShooters.enabled = false;
+        end.enabled = false;
+        colorful.enabled = false;
     }
 
     // Update is called once per frame
@@ -31,7 +48,71 @@ public class TextTrigger : MonoBehaviour
         if (collision.transform.CompareTag("Text Trigger"))
         {
             print(collision.gameObject.name);
-            colors.enabled = true;
+            if (collision.gameObject.name == "Colors")
+            {
+                colors.enabled = false;
+            }
+            if (collision.gameObject.name == "Welcome")
+            {
+                welcome.enabled = false;
+                jump.enabled = true;
+                checkp = false;
+            }
+            if (collision.gameObject.name == "Spike")
+            {
+                jump.enabled = false;
+                spike.enabled = true;
+            }
+            if (collision.gameObject.name == "SpikeBack")
+            {
+                spike.enabled = false;
+                spikeBack.enabled = true;
+            }
+            if (collision.gameObject.name == "Checkpoint")
+            {
+                spikeBack.enabled = false;
+                checkpoint.enabled = true;
+                checkp = true;
+            }
+            if (collision.gameObject.name == "Moving Platform")
+            {
+                checkpoint.enabled = false;
+                platforms.enabled = true;
+            }
+            if (collision.gameObject.name == "Spike Shooters")
+            {
+                platforms.enabled = false;
+                spikeShooters.enabled = true;
+            }
+            if (collision.gameObject.name == "End")
+            {
+                spikeShooters.enabled = false;
+                end.enabled = true;
+                colorful.enabled = true;
+            }
+        }
+        if (collision.transform.CompareTag("Finish"))
+        {
+            end.enabled = false;
+            colorful.enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("Death"))
+        {
+            print("died");
+            Start();
+            if (checkp)
+            {
+                welcome.enabled = false;
+                checkpoint.enabled = true;
+            }
+            else
+            {
+                welcome.enabled = true;
+            }
         }
     }
 }
