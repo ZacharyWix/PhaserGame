614b0049191f4bf3fcba69549e49e0966d67360f
Steven Mulvey <33791425+smulvey2@users.noreply.github.com>
2020-05-04 17:04:14 -0500

    Credits Functionality (needs new text)

---
+++ PhaserGame/Assets/Scripts/Credits.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public GameObject screen;
+    public GameObject credits;
+    private float scroll = 0;
+    private float speed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        credits.transform.position = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (screen.gameObject.activeSelf)
+        {
+            if (scroll <8)
+            {
+                scroll += speed;
+                credits.transform.position = new Vector2(0, scroll);
+            }
+        }
+    }
+
+    public void resetScroll()
+    {
+        scroll = 0;
+    }
+}
