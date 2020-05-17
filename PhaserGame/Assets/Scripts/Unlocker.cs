fb02f37597b4572a5becda4604fa2b2c10b9be8b
Steven Mulvey <33791425+smulvey2@users.noreply.github.com>
2020-05-17 12:30:32 -0500

    Achievement Popups

--- PhaserGame/Assets/Scripts/Unlocker.cs
+++ PhaserGame/Assets/Scripts/Unlocker.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Unlocker : MonoBehaviour
@@ -7,6 +8,10 @@ public class Unlocker : MonoBehaviour
     public SteamAchievements sa;
     private Achievement achievement;
     private level lv;
+    public GameObject achievement_00, achievement_01, achievement_02, achievement_03;
+    private bool showing = false;
+    private float timer = 0;
+    private GameObject active;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,27 +21,48 @@ public class Unlocker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (showing)
+        {
+            timer += 0.02f;
+            if(timer > 5)
+            {
+                timer = 0;
+                showing = false;
+                active.SetActive(false);
+            }
+        }
     }
 
     public void updateUnlocks()
     {
         if (level.getLevelDeaths(1) != -1 && level.getLevelDeaths(1) < 5)
         {
-            print("unlocking 0");
-            achievement = new Achievement(0);
-            sa.UnlockSteamAchievement("achievement_00");
+            unlock(achievement_00);
         }
         if (level.getLevelDeaths(2) != -1 && level.getLevelDeaths(2) < 5)
         {
-            print("unlocking 1");
-            achievement = new Achievement(1);
-            sa.UnlockSteamAchievement("achievement_01");
+            unlock(achievement_01);
         }
         if (level.getLevelDeaths(3) != -1 && level.getLevelDeaths(3) < 5)
         {
             print("unlocking 2");
             achievement = new Achievement(2);
+        }
+    }
+
+    public void unlock(GameObject ach)
+    {
+        char[] MyChar = { 'a', 'c', 'h', 'i', 'e', 'v', 'e', 'm', 'e', 'n', 't', '_' };
+        string a = ach.name.TrimStart(MyChar);
+        int x = Int32.Parse(a);
+        print(x);
+        if(!Achievement.getUnlocked(x))
+        {
+            achievement = new Achievement(x);
+            sa.UnlockSteamAchievement(ach.name);
+            ach.SetActive(true);
+            active = ach;
+            showing = true;
         }
     }
 }
