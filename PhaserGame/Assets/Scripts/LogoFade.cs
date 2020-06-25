682771019268d7898b7dcba00e7dd3a788ab3028
Steven Mulvey <33791425+smulvey2@users.noreply.github.com>
2020-06-25 03:23:07 -0500

    Logo Fade

--- PhaserGame/Assets/Scripts/LogoFade.cs
+++ PhaserGame/Assets/Scripts/LogoFade.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogoFade : MonoBehaviour
 {
-    private static bool loaded = false;
     public GameObject logo;
     public GameObject logoPanel;
+    public Image pic;
+    public Image background;
     private float timer = 0;
     private Vector3 inc;
+    public bool fade;
+    public bool up = false;
+    private float alpha = 0;
+    private float balpha = 1f;
     // Start is called before the first frame update
     void Start()
     {
         inc = new Vector3(0.001f, 0.001f, 0.001f);
-        if (!loaded)
+        if (!MainMenu.getLoaded())
         {
             logoPanel.SetActive(true);
         }
@@ -24,12 +30,53 @@ public class LogoFade : MonoBehaviour
     {
         if (logoPanel.activeSelf)
         {
-            logo.transform.localScale += inc;
-            timer += Time.deltaTime;
-            if(timer > 3)
+            if (fade)
             {
-                logoPanel.SetActive(false);
+                if (alpha < 1 && !up)
+                {
+                    alpha += 0.005f;
+                    var tempColor = pic.color;
+                    tempColor.a = alpha;
+                    pic.color = tempColor;
+                }
+                if (alpha >= 1 && !up)
+                {
+                    up = true;
+                }
+                if(alpha > 0 && up)
+                {
+                    alpha -= 0.005f;
+                    var tempColor = pic.color;
+                    tempColor.a = alpha;
+                    pic.color = tempColor;
+                }
+                if (alpha <= 0 && up)
+                {
+                    balpha -= 0.005f;
+                }
+                if(balpha < 1 && balpha > 0)
+                {
+                    balpha -= 0.005f;
+                    var tempColor1 = background.color;
+                    tempColor1.a = balpha;
+                    background.color = tempColor1;
+                }
+                if (balpha <= 0)
+                {
+                    logoPanel.SetActive(false);
+                    MainMenu.setLoaded(true);
+                }
             }
+            else
+            {
+                logo.transform.localScale += inc;
+                if (timer > 3)
+                {
+                    logoPanel.SetActive(false);
+                    MainMenu.setLoaded(true);
+                }
+            }
+            timer += Time.deltaTime;
         }
     }
 }
