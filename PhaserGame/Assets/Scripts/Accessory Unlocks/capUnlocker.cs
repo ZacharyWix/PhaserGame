fcaa50ea930835fad2682d755fdd45be5992d4aa
ZacharyWix <32174299+ZacharyWix@users.noreply.github.com>
2020-08-09 16:50:15 -0500

    Added Accessory Pickups

---
+++ PhaserGame/Assets/Scripts/Accessory Unlocks/capUnlocker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class capUnlocker : MonoBehaviour
+{
+    public Unlocker unlocks;
+    public GameObject accessory_4;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            unlocks.unlockAccessory(accessory_4);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ PhaserGame/Assets/Scripts/Accessory Unlocks/tophatUnlocker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tophatUnlocker : MonoBehaviour
+{
+    public Unlocker unlocks;
+    public GameObject accessory_3;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            unlocks.unlockAccessory(accessory_3);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
