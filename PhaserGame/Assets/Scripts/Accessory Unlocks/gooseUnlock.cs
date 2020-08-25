7aa4478e29ec70bcbbb3dd77872a9d0a05b0cddd
ZacharyWix <32174299+ZacharyWix@users.noreply.github.com>
2020-08-25 11:45:46 -0500

    Added Accessories and Fixed Camera Boundaries
    Added Accessories to: James_2, James_new, Joe_1, and Joe_2.
    
    Also fixed camera boundaries in those levels to ensure the area outside of the background can't be seen.

---
+++ PhaserGame/Assets/Scripts/Accessory Unlocks/gooseUnlock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gooseUnlock : MonoBehaviour
+{
+    public Unlocker unlocks;
+    public GameObject accessory_8;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            unlocks.unlockAccessory(accessory_8);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ PhaserGame/Assets/Scripts/Accessory Unlocks/scarfUnlocker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scarfUnlocker : MonoBehaviour
+{
+    public Unlocker unlocks;
+    public GameObject accessory_11;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            unlocks.unlockAccessory(accessory_11);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
